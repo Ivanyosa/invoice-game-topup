e6000e5b36b9d5749a67febfa747391d98f39730
Ivanyosa <150762684+Ivanyosa@users.noreply.github.com>
2026-01-19 08:04:57 +0700

    Update db_invoice_game.sql

--- sql/db_invoice_game.sql
+++ sql/db_invoice_game.sql
@@ -193,4 +193,10 @@ JOIN produk_topup p ON d.id_product = p.id_product;
 -- Satu produk dapat dibeli pada banyak transaksi
 -- Relasi M : N ini dipecah menggunakan tabel detail_transaksi
 
+3. Database dan seluruh tabel akan terbentuk otomatis
+4. Jalankan query SELECT untuk melihat hasil transaksi dan invoice
+
+---
+
+## 📁 Struktur Repository
 
